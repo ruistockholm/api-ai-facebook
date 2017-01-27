7a2230a92bd20f139ba0d09c6aae060211fcaaa2
ruistockholm <ruistockholm@gmail.com>
2017-01-27 08:11:55 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -353,55 +353,55 @@ function addGreetingText(){
 	
 	//-----------------------------
 	
-	app.post('/webhook', function (req, res) {
-  var data = req.body;
+	//app.post('/webhook', function (req, res) {
+ // var data = req.body;
 
   // Make sure this is a page subscription
-  if (data.object === 'page') {
+  //if (data.object === 'page') {
 
     // Iterate over each entry - there may be multiple if batched
-    data.entry.forEach(function(entry) {
-      var pageID = entry.id;
-      var timeOfEvent = entry.time;
+   // data.entry.forEach(function(entry) {
+    //  var pageID = entry.id;
+    //  var timeOfEvent = entry.time;
 
       // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
-        if (event.message) {
-          receivedMessage(event);
-        } else {
-          console.log("Webhook received unknown event: ", event);
-        }
-      });
-    });
+     // entry.messaging.forEach(function(event) {
+     //   if (event.message) {
+      //    receivedMessage(event);
+     //   } else {
+     //     console.log("Webhook received unknown event: ", event);
+  //      }
+  //    });
+  //  });
 
     // Assume all went well.
     //
     // You must send back a 200, within 20 seconds, to let us know
     // you've successfully received the callback. Otherwise, the request
     // will time out and we will keep trying to resend.
-    res.sendStatus(200);
-  }
-});
+  //  res.sendStatus(200);
+//  }
+//});
   
-function receivedMessage(event) {
+//function receivedMessage(event) {
   // Putting a stub for now, we'll expand it in the following steps
-  console.log("Message data: ", event.message);
-}
+  //console.log("Message data: ", event.message);
+//}
 	
-	function receivedMessage(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfMessage = event.timestamp;
-  var message = event.message;
+	//function receivedMessage(event) {
+  //var senderID = event.sender.id;
+  //var recipientID = event.recipient.id;
+  //var timeOfMessage = event.timestamp;
+  //var message = event.message;
 
-  console.log("Received message for user %d and page %d at %d with message:", 
-    senderID, recipientID, timeOfMessage);
-  console.log(JSON.stringify(message));
+  //console.log("Received message for user %d and page %d at %d with message:", 
+  //  senderID, recipientID, timeOfMessage);
+  //console.log(JSON.stringify(message));
 
-  var messageId = message.mid;
+  //var messageId = message.mid;
 
-  var messageText = message.text;
-  var messageAttachments = message.attachments;
+  //var messageText = message.text;
+  //var messageAttachments = message.attachments;
 
   if (messageText) {
 
